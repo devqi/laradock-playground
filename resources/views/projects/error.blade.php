db825d5d2178102af2bbd8567d2a042be9252285
QiTech <zhangq5721@gmail.com>
2019-07-10 15:31:11 +0200

    remove misplaced files

--- resources/views/projects/error.blade.php
+++
@@ -1,7 +0,0 @@
-@if ($errors->any())
-<div>
-    @foreach ($errors->all() as $error)
-    <p>{{ $error }}</p>
-    @endforeach
-</div>
-@endif
--- resources/views/projects/index.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('projects.layout')
-@section('title', 'All Project')
-
-@section('content')
-<h1>All Projects</h1>
-@foreach ($projects as $project)
-    <li><a href="/projects/{{ $project->id }}">{{ $project->title }}</a></li>
-@endforeach
-@endsection
